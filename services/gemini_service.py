a9b40e680aa65f5d593e9f4e0a4d58ae2b31387e
Ashwini Raj <ashwini.ash162@gmail.com>
2026-05-09 22:27:48 +0530

    fix: model name + dataset filename issues

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 
 def generate_response(user_prompt, system_prompt=""):
